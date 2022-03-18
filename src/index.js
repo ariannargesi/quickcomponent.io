29222687487ae5a8e7c71869c62022edadbb95bf
arian nargesi <ariannargesi@gmail.com>
2022-03-18 14:15:59 +0330

    Latest update

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import App from './App'
 import store from './redux'
 import 'antd/dist/antd.css';
 import {Provider} from 'react-redux'
-import { BrowserRouter, Route, Routes, HashRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Export from './component/Export'
 const root = document.getElementById('root')
 ReactDOM.render(<Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
     <Routes>
 
         <Route path="/" element={<App />} />
         <Route path="/export" element={<Export />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       
 </Provider>, root)
