2bf346e5778931741e81bc2e37c52ae51ee44aeb
arian nargesi <ariannargesi@gmail.com>
2022-04-12 13:04:47 +0430

    Showing proper messgae for mobile/tablet viewrs

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,25 @@ import "antd/dist/antd.min.css"
 import { Provider } from "react-redux"
 import { HashRouter, Route, Routes } from "react-router-dom"
 import Export from "./component/Export"
+import { useMediaQuery } from "react-responsive"
 const root = document.getElementById("root")
 
 const Main = () => {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: "(min-width: 1224px)",
+    })
+
+    if (!isDesktopOrLaptop)
+        return (
+            <div
+                style={{
+                    padding: "32px",
+                }}
+            >
+                <h2>Please visit this website with your desktop or laptop.</h2>
+                <h3>Come back. It's worth it 😉</h3>
+            </div>
+        )
     return (
         <Provider store={store}>
             <HashRouter>
