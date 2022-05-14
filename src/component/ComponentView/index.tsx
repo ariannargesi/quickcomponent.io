1de066ada579863248c4c094b0007f7a24a7f048
arian <ariannargesi@gmail.com>
2022-05-14 11:46:12 +0430

    Latest changes

--- src/component/ComponentView/index.tsx
+++ src/component/ComponentView/index.tsx
@@ -1,131 +1,24 @@
-import FormItemInput from 'antd/lib/form/FormItemInput'
-import React from 'react'
-import { AlignJustify } from 'react-feather'
-import { useDispatch, useSelector } from "react-redux"
-import {  isContentEditable } from '../../helper'
-import { changeSelectedElement, setInputAtKey, updateTreeInputValue, clearInputAtKey } from '../../redux/slice/app'
-import { RootState, ComponentMember } from "../../types"
-
-import styles from './styles.module.sass'
-
-
-
+import { useSelector } from "react-redux"
+import { RootState } from "../../types"
+import arrayToComponent from "../../helper/arrayToComponent"
+import styles from "./styles.module.sass"
 
 const ComponentView = () => {
-    const {map, inputKey, selectedKey} = useSelector((state: RootState) => state.app)
-    const dispatch = useDispatch()
-
-    const arrayToComponent = (map: ComponentMember[], fromInput = false): React.ReactNode => {
-        let temp = ''
-        const value = []
-        map.forEach((el) => {
-            if (inputKey === el.key) {
-                value.push(React.createElement(
-                    el.title,
-                    {
-                        style: el.props.style,
-                        key: el.key,
-                    },
-                    [
-                        React.createElement('input', 
-                            { 
-                                autoFocus: true, 
-                                
-                                className: styles.editInnerText,
-                                onKeyDown: (e ) => {
-                                    if(e.key === 'Enter')
-                                        dispatch(updateTreeInputValue({value: temp }))
-                                },
-                                onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-                                    const value = e.target.value 
-                                    temp = value 
-                                },
-                            
-                                onBlur: (e ) => {
-                                    if(temp) {
-                                        dispatch(updateTreeInputValue({value: temp }))
-                                    }
-                                        
-                                        
-                                },
-
-                            })
-                    ]
-                ))
-            }
-
-
-
-            else if (el.text) {
-                    if(el.title){
-                        value.push(React.createElement(
-                            el.title,
-                            {
-                                style: {...el.props.style, outlineColor: el.key === selectedKey ? 'lightgreen' : '#c9c9c9'},
-                                key: el.key,
-                                onClick: (e) => {
-                                    if (e.stopPropagation)
-                                        e.stopPropagation()
-                                    dispatch(changeSelectedElement({ key: el.key }))
-                                
-                                },
-                                onDoubleClick: (e) => {
-                                    if (e.stopPropagation)
-                                        e.stopPropagation()
-                                    if(isContentEditable(el.title))
-                                        dispatch(setInputAtKey({ key: el.key }))
-                                }
-                            },
-                            el.text
-                        ))
-                    }
-                    else {
-                        if(fromInput === false)
-                        value.push(el.text)
-                    }
-                    
-            }
-
-            else
-                value.push(React.createElement(
-                    el.title,
-                    {
-                        style: {...el.props.style, outlineColor: el.key === selectedKey ? 'lightgreen' : '#c9c9c9'},
-                        key: el.key,
-                        onClick: (e) => {
-                            if (e.stopPropagation)
-                                e.stopPropagation()
-                            dispatch(changeSelectedElement({ key: el.key }))
-                        
-                        },
-                        onDoubleClick: (e) => {
-                            if (e.stopPropagation)
-                                e.stopPropagation()
-                            if(isContentEditable(el.title))
-                                dispatch(setInputAtKey({ key: el.key }))
-                        }
-                    },
-                    arrayToComponent(el.children)
-                ))
-        })
-        return value
-    }
-
-    const component = arrayToComponent(map)
+    const { map, inputKey, selectedKey } = useSelector(
+        (state: RootState) => state.app
+    )
+    const component = arrayToComponent(map, inputKey, selectedKey)
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.header }>
+            <div className={styles.header}>
                 <h2>Component view</h2>
             </div>
-        <div className={styles.container}>
-            <div className={styles.content}>
-                {component}
+            <div className={styles.container}>
+                <div className={styles.content}>{component}</div>
             </div>
         </div>
-        </div>
     )
-        
 }
 
 export default ComponentView
