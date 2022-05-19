749cd962334992090c27fc57e56f57b08624c439
Ben Jesaulenko <benjezza@hotmail.com>
2022-05-19 15:15:18 +1000

    Add rem to fontSize - minor design changes

--- src/component/Selectors/FontSize.tsx
+++ src/component/Selectors/FontSize.tsx
@@ -1,19 +1,31 @@
 import styles from "./styles.module.sass"
+import style from "./styles.module.sass"
 import useApplyStyle from "../../hooks/useApplyStyle"
 import useStyleValue from "../../hooks/useStyleValue"
 import { getNumbericValue, getUnit } from "../../helper"
+import Radio from "../Radio"
 import Slider from "../Slider"
+const units = ["px", "rem",]
 
 const FontSize = () => {
     const applyStyle = useApplyStyle()
-    const fontSize = useStyleValue("fontSize") || "16px"
+    const fontSize = useStyleValue("fontSize")
     const fontSizeUnit = getUnit(fontSize)
     const fontSizeValue = getNumbericValue(fontSize)
 
     return (
         <div className={styles.container}>
+            <div className={style.header}>
+                    <span className={style.label}>Font Size:</span>
+                    <Radio
+                        options={units}
+                        activeItem={fontSizeUnit}
+                        onChange={(value) => {
+                            applyStyle("fontSize", fontSizeValue + value)
+                        }}
+                    />
+                </div>
             <Slider
-                label="Font size"
                 value={fontSizeValue}
                 onChange={(value) => {
                     applyStyle("fontSize", value + fontSizeUnit)
